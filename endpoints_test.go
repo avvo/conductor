bc739a827d8ff50c12910365c57ae2be0905ecca
Kyle Leaders <kyle.leaders@gmail.com>
2015-02-05 16:44:23 -0800

    Adding /_ping

---
+++ endpoints_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "net/http/httptest"
+  "net/http"
+  "testing"
+  "io/ioutil"
+)
+
+func init(){
+
+}
+
+func TestPingHandler(t *testing.T) {
+  ts := httptest.NewServer(http.HandlerFunc(pingHandler))
+  defer ts.Close()
+
+  res, err := http.Get(ts.URL)
+  if err != nil {
+    t.Fatal(err)
+  }
+  response, err := ioutil.ReadAll(res.Body)
+  res.Body.Close()
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if res.Status != "204 No Content" {
+    t.Errorf("Expected response status code to be 204 No Content, got '%v'", res.Status)
+  }
+
+  if string(response) != "" {
+    t.Errorf("Expected response to be: '', but got '%s'", response)
+  }
+}
